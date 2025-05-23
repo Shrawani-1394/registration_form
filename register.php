871011b8442af839ec048826042fee2baa260b3c
Shrawani <shrawanijagadale@gmail.com>
2025-05-23 23:13:59 +0530

    Initial commit - Online Admission Registration Form

---
+++ register.php
@@ -0,0 +1,37 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+// Connect to MySQL
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "registration_db";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// Get form data
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+
+  $sql = "INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')";
+
+  if ($conn->query($sql) === TRUE) {
+    echo "Registration successful!";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+
+  $conn->close();
+} else {
+  echo "Form not submitted.";
+}
+?>
